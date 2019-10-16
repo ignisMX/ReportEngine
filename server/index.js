c6f0ace11a0286b73fa0e091c93b803913b8252b
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2019-10-15 22:28:51 -0500

    Agrega respuesta de la raiz

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ import path from 'path';
 import express from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 40001;
+const PORT = process.env.PORT || 4001;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+//Root
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
 
 app.listen(PORT, (error) => {
     if(error){
