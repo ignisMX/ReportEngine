c54cbf649a0129ae89fb5718debe9c710deb1181
Omar Enrique Alvarez <ing.omar.alvarez@gmail.com>
2019-10-16 22:14:23 -0500

    cambia el nombre

--- src/barcode/index.js
+++ src/barcode/index.js
@@ -2,7 +2,7 @@ export const createLabel = (data) => {
     return `<div>${data.barcode}</div>`;
 }
 
-export const flat = ({data}) => {
+export const flatLabels = (data) => {
     let labels = [];
     data.forEach(element => {
         for (let index = 0; index < element.itemsQuantity; index++) {
